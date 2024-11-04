93f2663b76aa089eb8082f9e61cd65fc1c96c0fb
Rahul <rahulbodh819@gmail.com>
2024-11-04 18:00:48 +0530

    solve the equation

--- app/src/main/java/com/rahul/quardicequationsolverapp/MyEquation.kt
+++ app/src/main/java/com/rahul/quardicequationsolverapp/MyEquation.kt
@@ -1,5 +1,6 @@
 package com.rahul.quardicequationsolverapp
 
+import android.view.View
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import com.rahul.quardicequationsolverapp.databinding.ActivityMainBinding
@@ -25,4 +26,27 @@ class MyEquation : BaseObservable() {
         }
 
     private lateinit var binding : ActivityMainBinding
+
+    fun solveEquation(view : View) {
+
+        val a = a.toInt()
+        val b = b.toInt()
+        val c = c.toInt()
+
+        val discriminant = b*b - 4*a*c
+        if(discriminant < 0) {
+            binding.answerTextView.text = "No solution"
+
+        }else if(discriminant > 0) {
+            val root = Math.sqrt(discriminant.toDouble())
+            val x1 = (-b + root)/(2*a)
+            val x2 = (-b - root)/(2*a)
+
+            binding.answerTextView.text = "x1 = $x1, x2 = $x2"
+        }else {
+            val x = -b/(2*a)
+            binding.answerTextView.text = "x = $x"
+        }
+
+    }
 }
